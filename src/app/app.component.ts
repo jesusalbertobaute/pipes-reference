a2933f784fc2e34c20adc50086579602d3ce3e6d
jesusalbertobaute <jesusalbertobaute@gmail.com>
2022-02-17 07:55:29 -0300

    Cambios para Modulo de Pipes I18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api'; //Para efecto de splash de botones y Menu
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'pipesApp';
   nombre: string = 'hola mundo';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { ShareModule } from './share/share.module';
 import { VentasModule } from './ventas/ventas.module';
-
 
 //Cambiar el idioma de la app
 
@@ -21,8 +21,9 @@ registerLocaleData(localeFr);
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     ShareModule,
     VentasModule
   ],
